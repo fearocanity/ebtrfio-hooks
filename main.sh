ddb4808efb3d5bb0801915f67630042fd88ce65a
Fearocanity (けろさん) <91414643+fearocanity@users.noreply.github.com>
2023-11-04 09:46:59 +0900

    up folder

--- main.sh
+++ main.sh
@@ -3,8 +3,8 @@
 # variables
 token_fb="${1}"
 ggl_token="${2}"
-folder_ids=('1cwkdwlNKmJyF6RAwapv0Marpv1i_AOVh' '1jFMsiNLCQYKuBuAsBSdouFjafYiixist' '16R-L0OyZWTKmbZtrZJbTEDHBir9Jam4L' '1K1L0ddicvA5A4ek4mhrDZm5NeOq5UhmV' '1GrLgj9MR2HrbI3krLQpIa-M2In7_yiUA' '1pzTvM3EoEBBYjn8lcpbH66nyX0-oO2un' '1cidubuxt5dJSzO1O4VPJrYLfs47YKUXu' '1V8sK7UrFT9Jh3j4-4djVTvtmsOBSyWit' '1AVWaON7p50J9RFuCk5aMpAgtp8M3LgaB')
-frm_limt=('4322' '4264' '4971' '4971' '4972' '4975' '4975' '4975' '4975')
+folder_ids=('1cwkdwlNKmJyF6RAwapv0Marpv1i_AOVh' '1jFMsiNLCQYKuBuAsBSdouFjafYiixist' '16R-L0OyZWTKmbZtrZJbTEDHBir9Jam4L' '1K1L0ddicvA5A4ek4mhrDZm5NeOq5UhmV' '1GrLgj9MR2HrbI3krLQpIa-M2In7_yiUA' '1pzTvM3EoEBBYjn8lcpbH66nyX0-oO2un' '1cidubuxt5dJSzO1O4VPJrYLfs47YKUXu' '1V8sK7UrFT9Jh3j4-4djVTvtmsOBSyWit' '1AVWaON7p50J9RFuCk5aMpAgtp8M3LgaB' '1keG_8MiEOG3EldDA6XPEyrpcm8hECP0V')
+frm_limt=('4322' '4264' '4971' '4971' '4972' '4975' '4975' '4975' '4975' '4975')
 node="220726777558309/comments"
 base_urlorigin_fb="https://graph.facebook.com"
 base_urlorigin_ggl="https://www.googleapis.com"
